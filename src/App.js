6231bb9a20feba7c7ae5987c393feaf76f97391b
aboozarmojdeh <aboozar.mojdeh@gmail.com>
2020-09-18 14:02:59 -0700

    3 pages added for splitting

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      route: 'Page1'
+    }
+  }
+
+  onRouteChange = (route) => {
+    this.setState({ route: route })
+  }
+
+  render() {
+
+
+    if (this.state.route === 'Page1') {
+      return (
+        <Page1 onRouteChange={this.onRouteChange} />
+      )
+    } else if (this.state.route === 'Page2') {
+      return (
+        <Page2 onRouteChange={this.onRouteChange} />
+      )
+    } else if (this.state.route === 'Page3') {
+      return (
+        <Page3 onRouteChange={this.onRouteChange} />
+      )
+    }
+
+
+  }
+}
+
+export default App;
---
+++ src/components/Page2.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+const Page2=({onRouteChange})=>{
+    return(
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          
+        </header>
+        <button onClick={()=>onRouteChange('Page1')}>Page1</button>
+        <button className="disabled">Page2</button>
+        <button onClick={()=>onRouteChange('Page3')}>Page3</button>
+      </div>
+    )
+}
+
+export default Page2;
